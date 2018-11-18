caf8e854177b9adf14f48614e5c5877b079c46bd
Dmitriy Sedoy <dimon_85@hotmail.com>
2018-11-18 17:43:17 -0500

    fix error

--- client/src/App.js
+++ client/src/App.js
@@ -7,35 +7,41 @@ import 'bulma/css/bulma.min.css';
 
 class App extends React.Component {
   
-  state = {
-    doctors: null
-  }
-
-  getDoctor = async (e) => {
-    e.preventDefault();
-    const location = e.target.elements.location.value;
-    const specialty = e.target.elements.specialty.value;
-    const insurance = e.target.elements.insurance.value;
-    const api_call = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=${Config.API_KEY}&location=${location}&specialty_uid=${specialty}&insurance_uid=${insurance}`);
-    const data = await api_call.json();
-
-    this.setState({
-      doctors: data.data
-    });
-
-  }
 
   render() {
     return(
-      <div className="container">
+      
+    )
+  }
+}
+
+export default App;
+
+
+
+
+state = {
+  doctors: null
+}
+
+getDoctor = async (e) => {
+  e.preventDefault();
+  const location = e.target.elements.location.value;
+  const specialty = e.target.elements.specialty.value;
+  const insurance = e.target.elements.insurance.value;
+  const api_call = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=${Config.API_KEY}&location=${location}&specialty_uid=${specialty}&insurance_uid=${insurance}`);
+  const data = await api_call.json();
+
+  this.setState({
+    doctors: data.data
+  });
+
+}
+
+<div className="container">
         <Titles/>
         <br />
         <Form getDoctor={this.getDoctor}/>
         <br />
         <Doctors doctors={this.state.doctors}/>
       </div>
-    )
-  }
-}
-
-export default App;

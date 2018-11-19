6d978c7ffd6576fb9d26a8d8e3992a75e1711a85
Ana Triana <ana@ana3ana.com>
2018-11-19 16:08:39 -0500

    styled Titles

--- client/src/components/Titles.js
+++ client/src/components/Titles.js
@@ -1,9 +1,10 @@
 import React from "react";
+import '../styles/Titles.css'
 
 const Titles = () => (
 	<div className="hero has-text-centered ">
-		<h1 className="title ">MD Finder</h1>
-		<h3 className="subtitle">Find out  conditions and more...</h3>
+		<h1 className="title" > LET'S GET STARTED: </h1>
+		<h3 className="subheader">SEARCH FOR DOCTORS</h3>
 	</div>
 );
 

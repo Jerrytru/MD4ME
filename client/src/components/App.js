ca6ab886eb22fcc4faca0bb9f7e5490f09856786
John Doe <johndoe@example.com>
2018-11-19 16:44:58 -0500

    merging complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Titles from './Titles';
 import Form from './Form';
@@ -7,6 +6,12 @@ import Doctors from './Doctors';
 import Config from './Config';
 import 'bulma/css/bulma.min.css';
 import Body from './Body';
+import About from './About';
+import Contact from './Contact';
+import Language from './Language';
+import Ratings from './Ratings';
+import '../styles/global.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
 
@@ -33,6 +38,23 @@ class App extends React.Component {
   render() {
     return(
       <div>
+               <BrowserRouter className="App">
+     
+     <div class="Links">
+     <ul>
+            <li><Link to="/Language">Language</Link></li>
+            <li><Link to="/Ratings">Ratings</Link></li>
+            <li><Link to="/About" >About</Link></li>
+            <li><Link to="/Contact">Contact</Link></li>
+
+     </ul>
+        <Route path="/Language" component={Language} />
+        <Route path="/Ratings" component={Ratings} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+     </div>
+
+   </BrowserRouter>
         <Nav />
         <div className="container">
           <Titles/>
@@ -49,40 +71,3 @@ class App extends React.Component {
 }
 
 export default App;
-=======
-import React, { Component } from 'react';
-import About from './About';
-import Contact from './Contact';
-import Language from './Language';
-import Ratings from './Ratings';
-import '../styles/global.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-class App extends Component {
-
-
-    render() {
-        return (
-          <BrowserRouter className="App">
-     
-            <div class="Links">
-            <ul>
-                   <li><Link to="/Language">Language</Link></li>
-                   <li><Link to="/Ratings">Ratings</Link></li>
-                   <li><Link to="/About" >About</Link></li>
-                   <li><Link to="/Contact">Contact</Link></li>
-     
-            </ul>
-               <Route path="/Language" component={Language} />
-               <Route path="/Ratings" component={Ratings} />
-               <Route path="/About" component={About} />
-               <Route path="/Contact" component={Contact} />
-            </div>
-     
-          </BrowserRouter>
-        );
-        }
-    }
-
-    export default App;
->>>>>>> head
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import './components/index.css';
-=======
->>>>>>> head
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));

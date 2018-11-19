9023b17d5443a4713bc3dc36093752ef91598a10
John Doe <johndoe@example.com>
2018-11-18 21:04:26 -0500

    mergecss

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, Link, BrowserRouter} from 'react-router-dom';
-import Language from './components/Language';
-import Ratings from './components/Ratings';
+import App from './components/App';
 
-
-class Root extends React.Component {
-
-	render() {
-		return (
-			<BrowserRouter>
-				<div>
-					<div id="header">
-						<Link to="/Language">Language</Link>
-					</div>
-					<div>
-						<Link to="/Ratings">Ratings</Link>
-					</div>
-					<Route exact path="/Language" component={ Language } />
-					<Route exact path="/Ratings" component={ Ratings } />
-				</div>
-			</BrowserRouter>
-		)
-	}
-	
-}
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

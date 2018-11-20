3bf7e48ddb93701253bb24f58f2e1afeb0757a1e
Stommy123 <Stommy49@Yahoo.com>
2018-11-19 21:01:20 -0500

    resolved routing conflicts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import Titles from './Titles';
-import Form from './Form';
-import Nav from './Nav';
-import Doctors from './Doctors';
-import Config from './Config';
-import 'bulma/css/bulma.min.css';
-import Body from './Body';
+import Home from './Home.jsx'
+import Nav from './Nav'
 import About from './About';
 import Contact from './Contact';
 import Language from './Language';
@@ -14,105 +9,27 @@ import '../styles/global.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
-  
-  constructor() {
-    super();
-    this.state = {
-      doctors: [],
-      insurance: [],
-      selectedInsurance: null,
-      selectedInsurancePlans: [],
-      selectedPlanName: null,
-      selectedPlan: {}
-    }
-    this.updateSelectedInsurance = this.updateSelectedInsurance.bind(this);
-    this.updateSelectedPlan = this.updateSelectedPlan.bind(this);
-  }
-
-  updateSelectedInsurance(insuranceName) {
-    let foundInsurance = this.state.insurance.find((ins) => ins.name === insuranceName);
-    if(foundInsurance) {
-      this.setState({selectedInsurance: foundInsurance});
-      this.setState({selectedInsurancePlans: foundInsurance.plans});
-    }
-  }
-
-  updateSelectedPlan(planName) {
-    let selectedPlan = this.state.selectedInsurancePlans.find(plan => plan.name === planName);
-    if(selectedPlan) {
-      this.setState({selectedPlan: selectedPlan});
-    }
-  }
-
-
-  getDoctor = async (e) => {
-    e.preventDefault();
-
-    const state = document.getElementById('state-select').value;
-    console.log('State:', state);
-    const city = e.target.elements.city.value;
-    let location = "";
-    if(city) {
-      location = `${state}-${city}`.toLocaleLowerCase();
-    } else {
-      location = state.toLocaleLowerCase();
-    }
-    const specialty = e.target.elements.specialty.value;
-    const insurance = this.state.selectedPlan.uid;
-    const api_call = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=${Config.API_KEY}&location=${location}&specialty_uid=${specialty}&insurance_uid=${insurance}`);
-    const data = await api_call.json();
-
-    this.setState({
-      doctors: data.data
-    });
-
-  }
-
-  getInsurance = async (e) => {
-    const api_call = await fetch(`https://api.betterdoctor.com/2016-03-01/insurances?skip=0&user_key=${Config.API_KEY}`)
-    const {data} = await api_call.json()
-    return data;
-  }
-
-  async componentDidMount() {
-    let data = await this.getInsurance();
-    this.setState({insurance: data});
-  }
 
   render() {
+
     return(
-      <div>
-               <BrowserRouter className="App">
-     
-     <div class="Links">
-     <ul>
-            <li><Link to="/Language">Language</Link></li>
-            <li><Link to="/Ratings">Ratings</Link></li>
-            <li><Link to="/About" >About</Link></li>
-            <li><Link to="/Contact">Contact</Link></li>
-
-     </ul>
-        <Route path="/Language" component={Language} />
-        <Route path="/Ratings" component={Ratings} />
-        <Route path="/About" component={About} />
-        <Route path="/Contact" component={Contact} />
-     </div>
-
-   </BrowserRouter>
+    <BrowserRouter className="App">
+      <div class="Links">
         <Nav />
-        <div className="container">
-          <Titles/>
-          <br />
-          <Form getDoctor={this.getDoctor} getInsurance={this.getInsurance} 
-          insurances={this.state.insurance}
-          selectedPlans={this.state.selectedInsurancePlans}
-          updateSelectedInsurance={this.updateSelectedInsurance}
-          updateSelectedPlan={this.updateSelectedPlan}/>
-          <br />
-          <Doctors doctors={this.state.doctors}/>
-        </div>
-        <Body />
-      </div>
+      <ul>
+         <li><Link to="/">Home</Link></li>
+         <li><Link to="/Language">Language</Link></li>
+         <li><Link to="/Ratings">Ratings</Link></li>
+         <li><Link to="/About" >About</Link></li>
+         <li><Link to="/Contact">Contact</Link></li>
+      </ul>
+      <Route exact path="/" component={Home} />
+      <Route path="/Language" component={Language} />
+      <Route path="/Ratings" component={Ratings} />
+      <Route path="/About" component={About} />
+      <Route path="/Contact" component={Contact} />
+    </div>
+  </BrowserRouter>
 
     )
   }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import Titles from './Titles';
+import Form from './Form';
+import Nav from './Nav';
+import Doctors from './Doctors';
+import Config from './Config';
+import 'bulma/css/bulma.min.css';
+import Body from './Body';
+import '../styles/global.css';
+
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      doctors: [],
+      insurance: [],
+      selectedInsurance: null,
+      selectedInsurancePlans: [],
+      selectedPlanName: null,
+      selectedPlan: {}
+    }
+    this.updateSelectedInsurance = this.updateSelectedInsurance.bind(this);
+    this.updateSelectedPlan = this.updateSelectedPlan.bind(this);
+  }
+
+  updateSelectedInsurance(insuranceName) {
+    let foundInsurance = this.state.insurance.find((ins) => ins.name === insuranceName);
+    if(foundInsurance) {
+      this.setState({selectedInsurance: foundInsurance});
+      this.setState({selectedInsurancePlans: foundInsurance.plans});
+    }
+  }
+
+  updateSelectedPlan(planName) {
+    let selectedPlan = this.state.selectedInsurancePlans.find(plan => plan.name === planName);
+    if(selectedPlan) {
+      this.setState({selectedPlan: selectedPlan});
+    }
+  }
+
+
+  getDoctor = async (e) => {
+    e.preventDefault();
+
+    const state = document.getElementById('state-select').value;
+    console.log('State:', state);
+    const city = e.target.elements.city.value;
+    let location = "";
+    if(city) {
+      location = `${state}-${city}`.toLocaleLowerCase();
+    } else {
+      location = state.toLocaleLowerCase();
+    }
+    const specialty = e.target.elements.specialty.value;
+    const insurance = this.state.selectedPlan.uid;
+    const api_call = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?limit=10&user_key=${Config.API_KEY}&location=${location}&specialty_uid=${specialty}&insurance_uid=${insurance}`);
+    const data = await api_call.json();
+
+    this.setState({
+      doctors: data.data
+    });
+
+  }
+
+  getInsurance = async (e) => {
+    const api_call = await fetch(`https://api.betterdoctor.com/2016-03-01/insurances?skip=0&user_key=${Config.API_KEY}`)
+    const {data} = await api_call.json()
+    return data;
+  }
+
+  async componentDidMount() {
+    let data = await this.getInsurance();
+    this.setState({insurance: data});
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <Titles/>
+          <br />
+          <Form getDoctor={this.getDoctor} getInsurance={this.getInsurance}
+          insurances={this.state.insurance}
+          selectedPlans={this.state.selectedInsurancePlans}
+          updateSelectedInsurance={this.updateSelectedInsurance}
+          updateSelectedPlan={this.updateSelectedPlan}/>
+          <br />
+          <Doctors doctors={this.state.doctors}/>
+        </div>
+        <Body />
+      </div>
+
+    )
+  }
+}
+
+export default Home
